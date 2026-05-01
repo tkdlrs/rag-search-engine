f4d747eeb3c095eae9080ef6eec0a93e68aa35ed
tkdlrs <tkd.lsanchez@gmail.com>
2026-04-30 20:43:07 -0600

    Completes 'Chapter 1: Preprocessing | Lesson 4: Keyword Search' of the 'Learn Retrieval Augmented Generation' course on Boot.dev.

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,5 +1,33 @@
 import argparse 
+import json
+import os
+from pathlib import Path
 
+# 
+def get_data():
+    try:
+        data_file = Path(os.curdir) / "data" / "movies.json"
+        with open(data_file, mode="r") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        print(f"Error: file not found...")
+    except Exception as e:
+        print(f"Error of '{e}' occurred")
+    finally:
+        file.close()
+# 
+def keyword_search(search_term):
+    movies_data = get_data()
+    all_matches = list()
+    # 
+    for film in movies_data["movies"]:
+        title = film["title"]
+        if search_term in title:
+            all_matches.append(film)
+    # sort by ascending id
+    sorted_film_results = all_matches.sort(key=lambda x: (x['id']))
+    return all_matches[:5]
+# 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
@@ -12,7 +40,11 @@ def main() -> None:
     match args.command:
         case "search":
             # print the search query here
-            print(f"Searching for: {args.query}")
+            search_for = args.query
+            print(f"Searching for: {search_for}")
+            matches = keyword_search(search_for)
+            for i in range(len(matches)):
+                print(f"{i + 1}. {matches[i]["title"]}")
             pass
         case _:
             parser.print_help()
