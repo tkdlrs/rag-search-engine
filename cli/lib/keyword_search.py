79d10ed27f49bd60a8774d229d2b554b80354531
tkdlrs <tkd.lsanchez@gmail.com>
2026-05-02 17:51:00 -0600

    Completes 'Chapter 1: Preprocessing | Lesson 7: Tokenization' of the 'Learn Retrieval Augmented Generation' course on Boot.dev.

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -7,11 +7,14 @@ def search_command(query: str, limit: int = DEFAULT_SEARCH_LIMIT) -> list[dict]:
     results = []
     # 
     preprocessed_query = preprocess_text(query)
+    tokenized_query = tokenize(preprocessed_query)
     # 
     for movie in movies:
         title = movie["title"]
         preprocessed_title = preprocess_text(title)
-        if preprocessed_query in preprocessed_title:
+        tokenized_title = tokenize(preprocessed_title)
+        # if preprocessed_query in preprocessed_title:
+        if token_match(tokenized_query, tokenized_title):
             results.append(movie)
             if len(results) >= limit:
                 break    
@@ -24,4 +27,19 @@ def preprocess_text(text: str) -> str:
     text = text.translate(str.maketrans("", "", string.punctuation))
     # 
     return text 
-#  
+# 
+def tokenize(text: str) -> list[str]:
+    tokens = text.split(" ")
+    tokens = list(filter(None, tokens))
+    return tokens
+    
+# 
+def token_match(query_tokens: list[str], search_tokens: list[str]) -> bool: 
+    for q_token in query_tokens:
+        for s_token in search_tokens:
+            if q_token in s_token:
+                return True
+    # 
+    return False
+
+# 
