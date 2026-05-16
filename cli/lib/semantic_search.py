200fa0b6fa9165649605671268c3eaa6f8a9240a
tkdlrs <tkd.lsanchez@gmail.com>
2026-05-15 20:19:37 -0600

    This is were I got to on 'Chapter 4: Semantic Search | Lesson 11: Document Embeddings' in the 'Learn Retrieval Augmenetal Generation' course on Boot.dev.

--- cli/lib/semantic_search.py
+++ cli/lib/semantic_search.py
@@ -1,10 +1,23 @@
+import numpy as np 
+import os 
+# 
 from sentence_transformers import SentenceTransformer 
 # 
-# 
+from .search_utils import (
+    CACHE_DIR,
+    load_movies,
+
+)
 # 
 class SemanticSearch:
     def __init__(self, model_name="all-MiniLM-l6-v2") -> None:
         self.model = SentenceTransformer(model_name)
+        self.embeddings = None
+        self.documents = None
+        self.document_map = {}
+
+        self.movie_embeddings_path = os.path.join(CACHE_DIR, "movie_embeddings.npy")
+
         #
     def generate_embedding(self, text:str) -> str:
         if not text or not text.strip():
@@ -12,6 +25,45 @@ class SemanticSearch:
         # 
         embedding = self.model.encode([text])
         return embedding[0]
+    # 
+    def build_embeddings(self, documents):
+        self.documents = documents
+        # 
+        movie_list = []
+        # 
+        for doc in documents:
+            id = doc["id"]
+            self.document_map[id] = doc
+            # 
+            str_representation = f"{doc['title']}: {doc['description']}"
+            movie_list.append(str_representation)
+            # 
+        embeddings = self.model.encode(movie_list, show_progress_bar=True)
+        self.embeddings = embeddings
+        # 
+        with open(self.movie_embeddings_path, "wb") as f:
+            np.save(f, embeddings)
+        # 
+        return self.embeddings
+    # 
+    def load_or_create_embeddings(self, documents):
+        self.documents = documents
+        # 
+        for doc in documents:
+            id = doc["id"]
+            self.document_map[id] = doc 
+            # 
+        # 
+        if os.path.exists(self.movie_embeddings_path):
+            with open(self.movie_embeddings_path, "rb") as f:
+                self.embeddings = np.load(f)
+            if len(self.embeddings) == len(documents):
+                return self.embeddings
+            # 
+        else:
+            self.build_embeddings(documents)
+        # 
+
     # 
 # 
 def verify_model():
@@ -25,6 +77,12 @@ def embed_text(text:str):
     print(f"Text: {text}")
     print(f"First 3 dimensions: {embedding[:3]}")
     print(f"Dimensions: {embedding.shape[0]}")
-
+# 
+def verify_embeddings():
+    search_instance = SemanticSearch()
+    documents = load_movies()
+    embeddings = search_instance.load_or_create_embeddings(documents)
+    print(f"Number of docs: {len(documents)}")
+    print(f"Embeddings shape: {embeddings.shape[0]} vectors in {embeddings.shape[1]} dimensions")
 # 
 # 
--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -2,7 +2,7 @@
 
 import argparse 
 # 
-from lib.semantic_search import (verify_model, embed_text)
+from lib.semantic_search import (verify_model, embed_text, verify_embeddings)
 # 
 def main():
     parser = argparse.ArgumentParser(description="Semantic Search CLI")
@@ -13,6 +13,8 @@ def main():
     single_embed_parser = subparsers.add_parser("embed_text", help="Generate an embedding for a single text")
     single_embed_parser.add_argument("text", type=str, help="Text to embed")
     # 
+    subparsers.add_parser("verify_embeddings", help="Verify that the embeddings exist")
+    # 
     args = parser.parse_args()
     # 
     match args.command:
@@ -21,6 +23,8 @@ def main():
         case 'embed_text':
             text = args.text
             embed_text(text)
+        case 'verify_embeddings':
+            verify_embeddings()
         case _:
             parser.print_help()
 
