da23f2be8070b55f93baff8fa79b0d0dc416503e
tkdlrs <tkd.lsanchez@gmail.com>
2026-05-24 17:39:06 -0600

    Completes 'Chapter 6: Hybrid Search | Lesson 2: Hybrid Search' of the 'Learn Retrieval Augmented Generation' course on Boot.dev.

---
+++ cli/lib/hybrid_search.py
@@ -0,0 +1,27 @@
+import os
+#
+from .keyword_search import InvertedIndex
+from .semantic_search import ChunkedSemanticSearch
+#
+class HybridSearch:
+    def __init__(self, documents: list[dict]) -> None:
+        self.documents = documents
+        self.semantic_search = ChunkedSemanticSearch()
+        self.semantic_search.load_or_create_chunk_embeddings(documents)
+        #
+        self.idx = InvertedIndex()
+        #
+        if not os.path.exists(self.idx.index_path):
+            self.idx.build()
+            self.idx.save()
+        #
+        def _bm25_search(self, query: str, limit: int) -> list[dict]:
+            self.idx.load()
+            return self.idx.bm25_search(query, limit)
+        #
+        def weighted_search(self, query: str, alpha: float, limit: int = 5) -> list[dict]:
+            raise NotImplementedError("Weighted hybrid search is not implemented yet.")
+        #
+        def rrf_search(self, query: str, k: int, limit: int = 10) -> list[dict]:
+            raise NotImplementedError("RRF hybrid search is not implemented yet.")
+        #
