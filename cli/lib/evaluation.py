9acc2d9dedb13f6c189294325a4a4f640bb6b712
tkdlrs <tkd.lsanchez@gmail.com>
2026-06-04 20:42:03 -0600

    Completes 'Chapter 9: Evaluation | Lesson 4: Recall Metrics' of the 'Learn Retrieval Augmented Generation' course on Boot.dev.

--- cli/lib/evaluation.py
+++ cli/lib/evaluation.py
@@ -16,6 +16,15 @@ def precision_at_k(
             relevant_count += 1            
     return relevant_count / k 
 # 
+def recall_at_k(
+        retrieved_docs: list[str], relevant_docs: set[str], total_relevant: int, k: int = 5,  
+) -> float:
+    top_k = retrieved_docs[:k]
+    relevant_count = 0 
+    for doc in top_k:
+        if doc in relevant_docs:
+            relevant_count += 1            
+    return relevant_count / total_relevant 
 # 
 def evaluate_command(limit: int = 5) -> dict:
     movies = load_movies()
@@ -39,9 +48,12 @@ def evaluate_command(limit: int = 5) -> dict:
                 retrieved_docs.append(title)
         # 
         precision = precision_at_k(retrieved_docs, relevant_docs, limit)
+        recall = recall_at_k(retrieved_docs, relevant_docs, len(relevant_docs) ,limit)
+         
         # 
         results_by_query[query] = {
             "precision": precision,
+            "recall": recall,
             "retrieved": retrieved_docs[:limit],
             "relevant": list(relevant_docs),
         }
