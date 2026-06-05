2c5e96e9b9d1eede897f996455d270a7ed02baf8
tkdlrs <tkd.lsanchez@gmail.com>
2026-06-04 20:20:05 -0600

    Reviews the solution files for 'Chapter 9: Evaluation | Lesson 3: Precision Metrics' of the 'Learn Retrieval Augmented Generation' course on Boot.dev.

--- cli/evaluation_cli.py
+++ cli/evaluation_cli.py
@@ -1,7 +1,6 @@
 import argparse
 # 
-from lib.search_utils import load_golden_dataset
-from lib.hybrid_search import rrf_search_command
+from lib.evaluation import evaluate_command 
 
 # 
 def main() -> None:
@@ -14,28 +13,16 @@ def main() -> None:
     )
     # 
     args = parser.parse_args()
-    limit = args.limit 
-
-    #
-    golden_dataset = load_golden_dataset()
+    result = evaluate_command(args.limit)
     # 
-    print(f"k={limit}")
-    for test_case in golden_dataset['test_cases']:
-        print(f"- Query: {test_case['query']}")
-        result = rrf_search_command( test_case['query'], 60, limit=limit)
-        retrieved_titles = ""
-        for i, res in enumerate(result['results'], 1):
-            if i == len(result['results']):
-                retrieved_titles += f"{res['title']}"
-            else:
-                retrieved_titles += f"{res['title']}, "
-        # 
-        # print(f" wtc is result? {result}")
-        print(f"   - Precision@{limit}: { ( len(test_case['relevant_docs']) / len(result['results']) ):.4f}")
-        print(f"   - Retrieved: {retrieved_titles}")
-        print(f"   - Relevant: {", ".join(test_case['relevant_docs'])}")
-        
-    
+    print(f"k={args.limit}")
+    for query, res in result["results"].items():
+        print(f"- Query: {query}")
+        print(f"   - Precision@{args.limit}: {res['precision']:.4f}")
+        print(f"   - Retrieved: {', '.join(res['retrieved'])}")
+        print(f"   - Relevant: {', '.join(res['relevant'])}")
+        print()
+    # 
 # 
 if __name__ == "__main__":
     main()
