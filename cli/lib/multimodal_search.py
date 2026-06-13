e23090cdb8e6624d3e455d27d53f2257866d1960
tkdlrs <tkd.lsanchez@gmail.com>
2026-06-12 21:21:19 -0600

    Starts 'Chapter 12: Multimodal | Lesson 3: Image Embeddings' of the 'Learn Retrieval Augmented Generation' course on Boot.dev.

---
+++ cli/lib/multimodal_search.py
@@ -0,0 +1,21 @@
+import os
+# 
+from PIL import Image
+from sentence_transformers import SentenceTransformer
+# 
+class MultimodalSearch:
+    def __init__(self, model_name="clip-ViT-B-32"):
+        self.model = SentenceTransformer(model_name)
+    # 
+    def embed_image(self, image_path: str):
+         if not os.path.exists(image_path):
+             raise FileNotFoundError(f"Image file not found: {image_path}")
+         #  
+         image = Image.open(image_path)
+         return self.model.encode(image)[0]
+#         
+# 
+def verify_image_embedding(image_path):
+    multimodalSearch = MultimodalSearch()
+    embedding = multimodalSearch.embed_image(image_path)
+    print(f"Embedding shape: {embedding.shape[0]} dimensions")
---
+++ cli/multimodal_search_cli.py
@@ -0,0 +1,31 @@
+import argparse
+import os
+# 
+from lib.multimodal_search import verify_image_embedding
+#  
+#  
+def main() -> None:
+    parser = argparse.ArgumentParser(description="Image + text -> rewritten query")
+    # 
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+    # 
+    verify_image_parser = subparsers.add_parser("verify_image_embedding", help="Verify that the embedded model is loaded")
+    verify_image_parser.add_argument("image", type=str, help="Path to image file")
+    # 
+    args = parser.parse_args()
+    # 
+    #
+    match args.command:
+        case 'verify_image_embedding':
+            if not os.path.exists(args.image):
+                raise FileNotFoundError(f"Image file not found: {args.image}")
+            res = verify_image_embedding(args.image)
+            print(res)
+        # 
+        case _:
+            parser.print_help()
+   #
+# 
+if __name__ == "__main__":
+    main()
+    
