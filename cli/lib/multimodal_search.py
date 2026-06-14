f173505b98a1ab0d1dcbebc29c22459fe33609cd
tkdlrs <tkd.lsanchez@gmail.com>
2026-06-13 18:49:30 -0600

    Reviews the solution files for 'Chapter 12: Multimodal | Lesson 3: Image Embeddings' of the 'Learn Retrieval Augmented Generation' course on Boot.dev.

--- cli/lib/multimodal_search.py
+++ cli/lib/multimodal_search.py
@@ -1,22 +1,29 @@
 import os
+from typing import Any
 # 
 from PIL import Image
+from numpy.typing import NDArray
 from sentence_transformers import SentenceTransformer
+#
 # 
 class MultimodalSearch:
-    def __init__(self, model_name="clip-ViT-B-32"):
+    def __init__(
+            self, documents: list[object] | None = None, model_name: str = "clip-ViT-B-32"
+    ) -> None:
+        if documents is None:
+            documents = []
         self.model = SentenceTransformer(model_name)
     # 
-    def embed_image(self, image_path: str):
+    def embed_image(self, image_path: str) -> NDArray[Any]:
          if not os.path.exists(image_path):
              raise FileNotFoundError(f"Image file not found: {image_path}")
-         #  
          image = Image.open(image_path)
-         return self.model.encode([image])[0]
+         image_embedding = self.model.encode([image]) # type: ignore[arg-type]
+         return image_embedding[0]
 #         
 # 
-def verify_image_embedding(image_path):
-    multimodalSearch = MultimodalSearch()
-    embedding = multimodalSearch.embed_image(image_path)
+def verify_image_embedding(image_path: str) -> None:
+    searcher = MultimodalSearch()
+    embedding = searcher.embed_image(image_path)
     print(f"Embedding shape: {embedding.shape[0]} dimensions")
 
--- cli/multimodal_search_cli.py
+++ cli/multimodal_search_cli.py
@@ -1,30 +1,28 @@
+#! /usr/bin/env python3
+# 
 import argparse
-import os
 # 
 from lib.multimodal_search import verify_image_embedding
 #  
-#  
+#
 def main() -> None:
-    parser = argparse.ArgumentParser(description="Image + text -> rewritten query")
-    # 
+    parser = argparse.ArgumentParser(description="Multimodal Search CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
     # 
-    verify_image_parser = subparsers.add_parser("verify_image_embedding", help="Verify that the embedded model is loaded")
+    verify_image_parser = subparsers.add_parser(
+        "verify_image_embedding", help="Verify image embedding"
+    )
     verify_image_parser.add_argument("image", type=str, help="Path to image file")
     # 
     args = parser.parse_args()
-    # 
     #
     match args.command:
         case 'verify_image_embedding':
-            if not os.path.exists(args.image):
-                raise FileNotFoundError(f"Image file not found: {args.image}")
             verify_image_embedding(args.image)
-        # 
         case _:
             parser.print_help()
    #
 # 
 if __name__ == "__main__":
     main()
-    
+#  
