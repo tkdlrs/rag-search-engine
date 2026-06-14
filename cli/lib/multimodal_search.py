fd47958bc82dfa7048375000c6fa646da5339faf
tkdlrs <tkd.lsanchez@gmail.com>
2026-06-13 19:47:45 -0600

    Completes 'Chapter 12: Multimodal | Lesson 4: Multimodal Search Implementation' in the 'Learn Retrieval Augmented Generation' course on Boot.dev. This is the final lesson in this course.

--- cli/lib/multimodal_search.py
+++ cli/lib/multimodal_search.py
@@ -4,6 +4,9 @@ from typing import Any
 from PIL import Image
 from numpy.typing import NDArray
 from sentence_transformers import SentenceTransformer
+# 
+from lib.search_utils import Movie, load_movies
+from lib.semantic_search import cosine_similarity
 #
 # 
 class MultimodalSearch:
@@ -12,7 +15,13 @@ class MultimodalSearch:
     ) -> None:
         if documents is None:
             documents = []
+        self.documents = documents
+        self.texts: list[str] = []
+        for doc in documents:
+            self.texts.append(f"{doc['title']}: {doc['description']}")
+        # 
         self.model = SentenceTransformer(model_name)
+        self.text_embeddings = self.model.encode(self.texts, show_progress_bar=True)
     # 
     def embed_image(self, image_path: str) -> NDArray[Any]:
          if not os.path.exists(image_path):
@@ -20,10 +29,40 @@ class MultimodalSearch:
          image = Image.open(image_path)
          image_embedding = self.model.encode([image]) # type: ignore[arg-type]
          return image_embedding[0]
+    # 
+    def search_with_image(self, image_path: str) -> list[dict]:
+        image_embedding = self.embed_image(image_path)
+        # 
+        similarities: list[tuple[float, Movie]] = []
+        for i, text_embedding in enumerate(self.text_embeddings):
+            similarity = cosine_similarity(text_embedding, image_embedding )
+            similarities.append((similarity, self.documents[i]))
+        #          
+        similarities.sort(key=lambda x: x[0], reverse=True)
+        # 
+        results = []
+        for i, similarity in enumerate(similarities):
+            if i >= 5: break
+            # 
+            results.append({  
+                "id": similarity[1]["id"],
+                "title": similarity[1]["title"],
+                "description": similarity[1]["description"],
+                "similarity_score": similarity[0],
+            })
+        # 
+        return results
 #         
 # 
 def verify_image_embedding(image_path: str) -> None:
     searcher = MultimodalSearch()
     embedding = searcher.embed_image(image_path)
     print(f"Embedding shape: {embedding.shape[0]} dimensions")
-
+# 
+# 
+def image_search_command(image_path: str) -> list[object]:
+    movies = load_movies()
+    searcher = MultimodalSearch(movies)
+    return searcher.search_with_image(image_path)
+# 
+# 
--- cli/multimodal_search_cli.py
+++ cli/multimodal_search_cli.py
@@ -2,7 +2,7 @@
 # 
 import argparse
 # 
-from lib.multimodal_search import verify_image_embedding
+from lib.multimodal_search import image_search_command, verify_image_embedding
 #  
 #
 def main() -> None:
@@ -14,11 +14,23 @@ def main() -> None:
     )
     verify_image_parser.add_argument("image", type=str, help="Path to image file")
     # 
+    search_image_parser = subparsers.add_parser(
+        "image_search", help="searach image"
+    )
+    search_image_parser.add_argument("image", type=str, help="Path to image file")
+    # 
     args = parser.parse_args()
     #
     match args.command:
         case 'verify_image_embedding':
             verify_image_embedding(args.image)
+        case 'image_search':
+            results = image_search_command(args.image)
+            for i, result in enumerate(results, 1):
+                print(f"{i}.   {result['title']} (similarity: {result['similarity_score']:.4f})")
+                print(f"     {result['description'][:100]}")
+                print()
+
         case _:
             parser.print_help()
    #
